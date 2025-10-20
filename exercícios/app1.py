79455762f8d6c74facfc045354cfb391d5282926
RAFAELI GEHLEN DA SILVA <rafaeli_g_silva@estudante.sesisenai.org.br>
2025-10-20 10:08:30 -0300

    Adicionando arquivos à nova pasta

--- exercícios/app1.py
+++ exercícios/app1.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('indexexercicio1.html')
+    return render_template('index.html')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobreexercicio1.html')
+    return render_template('sobre.html')
 
 @app.route('/contatos')
 def contatos():
-    return render_template('contatosexercicio1.html')
+    return render_template('contatos.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
